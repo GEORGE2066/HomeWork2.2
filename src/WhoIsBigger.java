358906218cedd6c3bad5a040f27fb90275ec713c
Администратор <george2066@yandex.ru>
2022-09-13 17:55:23 +0300

    Второе домашнее задание второго курса

---
+++ src/WhoIsBigger.java
@@ -0,0 +1,46 @@
+public class WhoIsBigger {
+    public static String WhoIsBiggerGriff(Griffindor a, Griffindor b) {
+        if (a.getSum() > b.getSum()) {
+            return "Лучше " + a.getName();
+        } else if(a.getSum() < b.getSum()) {
+            return "Лучше " + b.getName();
+        } else {
+            return "Они равны";
+        }
+    }
+
+    public static String WhoIsBiggerPuff(Puffenduy a, Puffenduy b) {
+        if (a.getSum() > b.getSum()) {
+            return "Лучше " + a.getName();
+        } else if(a.getSum() < b.getSum()) {
+            return "Лучше " + b.getName();
+        } else {
+            return "Они равны";
+        }
+    }
+
+    public static String WhoIsBiggerCokte(Coktevran a, Coktevran b) {
+        if (a.getSum() > b.getSum()) {
+            return "Лучше " + a.getName();
+        } else if(a.getSum() < b.getSum()) {
+            return "Лучше " + b.getName();
+        } else {
+            return "Они равны";
+        }
+    }
+
+    public static String WhoIsBiggerSlith(Slitherin a, Slitherin b) {
+        if (a.getSum() > b.getSum()) {
+            return "Лучше " + a.getName();
+        } else if(a.getSum() < b.getSum()) {
+            return "Лучше " + b.getName();
+        } else {
+            return "Они равны";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
